276e30f6a26ebd25fa8521d405c714310f4e5f1e
Tommie <Tommie@User-PC>
2015-05-01 02:20:48 +0100

    Added more function to the DiaryServer

---
+++ src/java/serverPackage/DiaryWebService.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverPackage;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author maxamud
+ */
+@WebService(serviceName = "DiaryWebService")
+public class DiaryWebService {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "clientName")
+    public String clientName(@WebParam(name = "clientName") String clientName) {
+        return "Hello " + clientName + " !, Welcome to MaxDiary.com";
+    }
+
+    @WebMethod(operationName = "setDate")
+    public String setDate(@WebParam(name = "setDate") String date) {
+        return "This event happened on the:" + date;
+    }
+
+    @WebMethod(operationName = "setClientNote")
+    public String setClientNote(@WebParam(name = "setNote") String setNote) {
+        return "Note as follow" + setNote;
+    }
+}
